a16a25ee76c0c11b98571730b8cb5277617f77c9
Csyhd <csyhd@msn.com>
2020-12-03 21:52:45 +0800

    feat:created project init and add show time,method,url function

---
+++ app.js
@@ -0,0 +1,35 @@
+// app.js
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const today = new Date() //建立Date物件
+  console.log(`${today.toString()} | ${req.method} from ${req.url}`)
+  next()
+})
+
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
