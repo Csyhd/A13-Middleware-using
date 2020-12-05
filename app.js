f5336a653f112c9638f3a1d9f41ce96712e7ec21
Csyhd <csyhd@msn.com>
2020-12-05 18:22:05 +0800

    feat:add show res time and req time

--- app.js
+++ app.js
@@ -7,14 +7,27 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use((req, res, next) => {
-  const today = new Date() //建立Date物件
-  console.log(`${today.toString()} | ${req.method} from ${req.url}`)
+const requestTime = function (req, res, next) {
+  req.requestTime = new Date()
   next()
-})
+}
 
+app.use(requestTime)
 
 app.get('/', (req, res) => {
+  const reqTime = req.requestTime
+  const resTime = new Date()
+  const totalTime = Number(resTime.getTime()) - Number(req.requestTime.getTime())
+
+  //show req time
+  console.log(`Request  time : ${reqTime.toLocaleDateString()} ${reqTime.toLocaleTimeString()} (${reqTime.getTime()} ms) | ${req.method} from ${req.url}`)
+
+  //show res time 
+  console.log(`Response time : ${resTime.toLocaleDateString()} ${resTime.toLocaleTimeString()} (${resTime.getTime()} ms) | ${req.method} from ${req.url}`)
+
+  //show total time
+  console.log(` Total time: ${totalTime}`)
+
   res.send('列出全部 Todo')
 })
 
